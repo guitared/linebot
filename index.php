e38d7a3f0ac5c005909793b6312161326ddef692
Thatchapatt Kesornsri <guitared@users.noreply.github.com>
2016-11-08 19:18:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ require_once('./LINEBotTiny.php');
 $channelAccessToken = '4njqcB7ce+FZeLMAil5vte0VeoyvHGBkSO3HcM6bPOT7oDoDD/5jxD/aboxpOptqM7CeCantL2krpN3flrSJltb6hK5VT5MUlpaQ51SSc7y1pVNQI/JGYiCeYG0rK+uHWvnCil+WTJXhJw69CehD7AdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'd7a05537c9cc008dfe153c66dbbd0583';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-echo "index";
-var_dump($client);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':

6de658f65c4213b0016c2eb431b9f6fbb8dfe522
Thatchapatt Kesornsri <guitared@users.noreply.github.com>
2016-11-08 18:59:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
-header('Content-type:application/json;charset=utf-8');
-echo "hello world";
-$path = $_SERVER['REQUEST_URI']?:'/';
-var_dump(explode('/',$path));
+require_once('./LINEBotTiny.php');
+$channelAccessToken = '4njqcB7ce+FZeLMAil5vte0VeoyvHGBkSO3HcM6bPOT7oDoDD/5jxD/aboxpOptqM7CeCantL2krpN3flrSJltb6hK5VT5MUlpaQ51SSc7y1pVNQI/JGYiCeYG0rK+uHWvnCil+WTJXhJw69CehD7AdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'd7a05537c9cc008dfe153c66dbbd0583';
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};

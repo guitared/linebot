f734347dbcf28be9819fda6efd32a6199b2e1055
Thatchapatt Kesornsri <guitared@users.noreply.github.com>
2016-11-10 15:28:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once('./LINEBotTiny.php');
 $channelAccessToken = '4njqcB7ce+FZeLMAil5vte0VeoyvHGBkSO3HcM6bPOT7oDoDD/5jxD/aboxpOptqM7CeCantL2krpN3flrSJltb6hK5VT5MUlpaQ51SSc7y1pVNQI/JGYiCeYG0rK+uHWvnCil+WTJXhJw69CehD7AdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'd7a05537c9cc008dfe153c66dbbd0583';
@@ -9,12 +10,13 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
+                    if($message['text']=="r")session_regenerate_id();
                     $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $message['text']
+                                'text' => $message['text'].'\nsession_id:'.session_id()
                             )
                         )
                     ));
